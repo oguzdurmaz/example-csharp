9d3a3c5d1581d0c9989c00a703ac992cd5fcadd8
oguzdurmaz <43408707+oguzdurmaz@users.noreply.github.com>
2018-09-20 16:15:27 +0300

    Class constants is changed.

--- MyUnitTests/MyTargetClassTests.cs
+++ MyUnitTests/MyTargetClassTests.cs
@@ -28,22 +28,23 @@ namespace MyUnitTests
         [Fact]
         public void VerifyDivide()
         {
-            const int intFirstNumber = 15;
-            const int intsecondNumber = 10;
+            const int intFirstNumber = 25;
+            const int intsecondNumber = 20;
             var objTargetClass = new MyTargetClass();
             var intResult = objTargetClass.DivideNumbers(intFirstNumber, intsecondNumber);
-            Assert.Equal(15 / 10, intResult);
+            Assert.Equal(25 / 20, intResult);
         }
 
         [Fact]
         public void VerifyMultiple()
         {
-            const int intFirstNumber = 15;
-            const int intsecondNumber = 10;
+            const int intFirstNumber = 35;
+            const int intsecondNumber = 30;
             var objTargetClass = new MyTargetClass();
             var intResult = objTargetClass.MultipleNumbers(intFirstNumber, intsecondNumber);
-            Assert.Equal(15 * 10, intResult);
+            Assert.Equal(35 * 30, intResult);
         }
+
     }
 
     }
